449bbc3bfe5ce85c1f0fc6bc3fda6fdde6c1169f
Александр <matruk.aleksandr@gmail.com>
2022-03-12 16:46:34 +0300

    1-2 задания + 3 задание с "багом"

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,13 +3,15 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
+
+        //задание 1
         int clientOS = 1;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке ");
         } else {
             System.out.println("Установите версию приложения для AndroidOS по ссылке");
         }
-
+        //задание 2
         int clientDeviceYear = 2014;
         boolean versionPO = clientDeviceYear <= 2014 && clientOS == 0;
 
@@ -19,5 +21,23 @@ public class Main {
         if( versionPO = clientDeviceYear <= 2014 && clientOS == 1){
             System.out.println("Установите облегченную версию приложения для AndroidOS по ссылке");
         }
+        System.out.println();
+
+        //задание 3
+        int year = 100;
+
+        if (year % 4 == 0) {
+            if ((year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(year + " год является високосным!");
+            }
+        } else System.out.println(year + " год не является високосным!");
+
+
+
+
+
+
     }
-}
+
+    }
+
