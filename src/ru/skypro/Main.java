e36271f66052d3fd446d91802cd1bb97c7cbd715
Александр <matruk.aleksandr@gmail.com>
2022-03-15 20:17:50 +0300

    1-2 задания + 3 задание + 4 задание + 5 задание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -44,14 +44,59 @@ public class Main {
         }
 
          if (deliveryDistance >60 && deliveryDistance < 100) { deliveryDay = deliveryDay+2;
-            System.out.println("Потребуется дней: " + deliveryDay); }
-        }
+            System.out.println("Потребуется дней: " + deliveryDay);
+         }
 
+         // задание 5
 
+        int monthNumber = 12;
+         switch (monthNumber) {
+             case 1:
+                 System.out.println("Январь принадлежит к сезону ЗИМА");
+                 break;
+             case 2:
+                 System.out.println("Февраль принадлежит к сезону ЗИМА");
+                 break;
+             case 3:
+                 System.out.println("Март принадлежит к сезону ВЕСНА");
+                 break;
+             case 4:
+                 System.out.println("Апрель принадлежит к сезону ВЕСНА");
+                 break;
+             case 5:
+                 System.out.println("Май принадлежит к сезону ВЕСНА");
+                 break;
+             case 6:
+                 System.out.println("Июнь принадлежит к сезону ЛЕТО");
+                 break;
+             case 7:
+                 System.out.println("Июль принадлежит к сезону ЛЕТО");
+                 break;
+             case 8:
+                 System.out.println("Август принадлежит к сезону ЛЕТО");
+                 break;
+             case 9:
+                 System.out.println("Сентябрь принадлежит к сезону ОСЕНЬ");
+                 break;
+             case 10:
+                 System.out.println("Октябрь принадлежит к сезону ОСЕНЬ");
+                 break;
+             case 11:
+                 System.out.println("Ноябрь принадлежит к сезону ОСЕНЬ");
+                 break;
+             case 12:
+                 System.out.println("Декабрь принадлежит к сезону ЗИМА");
+                 break;
+             default:
+                 System.out.println("Это точно не земной год!");
+
+         }
+         }
 
 
 
 
     }
+
 
 
